00ca693767546d6a4b84a2dda7229ec6baa5f94c
Ed Stephinson <edward.stephinson@gmail.com>
2023-07-10 21:58:46 +0100

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,50 +1,45 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "Sailhouse",
-      head: [
-        {
-          tag: "link",
-          attrs: {
-            rel: "shortcut icon",
-          },
-          content: "/favicon.ico",
-        },
-      ],
-      logo: {
-        src: "/src/assets/logo.svg",
+  integrations: [starlight({
+    title: "Sailhouse",
+    head: [{
+      tag: "link",
+      attrs: {
+        rel: "shortcut icon"
       },
-      social: {
-        github: "https://github.com/withastro/starlight",
-      },
-      customCss: ["/src/styles/main.css"],
-      sidebar: [
-        {
-          label: "Getting Started",
-          items: [
-            {
-              label: "Setup",
-              link: "/getting-started/setup/",
-            },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: {
-            directory: "reference",
-          },
-        },
-      ],
-    }),
-  ],
+      content: "/favicon.ico"
+    }],
+    logo: {
+      src: "/src/assets/logo.svg"
+    },
+    social: {
+      github: "https://github.com/withastro/starlight"
+    },
+    customCss: ["/src/styles/main.css"],
+    sidebar: [{
+      label: "Getting Started",
+      items: [{
+        label: "Setup",
+        link: "/getting-started/setup/"
+      }]
+    }, {
+      label: "Reference",
+      autogenerate: {
+        directory: "reference"
+      }
+    }]
+  })],
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
   image: {
     service: {
-      entrypoint: "astro/assets/services/sharp",
-    },
+      entrypoint: "astro/assets/services/sharp"
+    }
   },
+  output: "server",
+  adapter: netlify()
 });
