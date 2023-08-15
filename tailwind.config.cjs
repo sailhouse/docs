8b7456e98e164fcdfdfbf402556a0477e5f982c3
Ed Stephinson <edward.stephinson@gmail.com>
2023-08-15 09:37:54 +0100

    Small style changes, add discord

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+const starlightPlugin = require("@astrojs/starlight-tailwind");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        // Your preferred accent color. Indigo is closest to Starlight’s defaults.
+        accent: colors.orange,
+        // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
+        //   gray: colors.zinc,
+      },
+      fontFamily: {
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+        mono: ["Source Code Pro", "monospace"],
+      },
+    },
+  },
+  plugins: [starlightPlugin()],
+};
